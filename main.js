b5684f628d044ea2007b71342efe5fe57f3fcb84
ananyaaverineni <78314919+ananyaaverineni@users.noreply.github.com>
2021-06-25 11:57:09 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+mouseEvent="empty";
+var last_position_of_x,last_position_of_y
+canvas=document.getElementById('myCanvas')
+ctx=canvas.getContext("2d");
+color="black";
+width_of_line=1;
+  
+canvas.addEventListener("mousedown",my_mousedown )
+function my_mousedown (e){
+    mouseEvent="mousedown"
+}
+canvas.addEventListener("mouseup",my_mouseup )
+function my_mouseup (e){
+    mouseEvent="mouseup"
+}
+canvas.addEventListener("mouseleave",my_mouseleave )
+function my_mouseleave (e){
+    mouseEvent="mouseleave"
+}
+canvas.addEventListener("mousemove",draw_line);
+function draw_line (e)
+{current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+ current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+ if (mouseEvent=="mousedown")
+{
+ ctx.beginPath()
+ ctx.strokeStyle=color;
+ ctx.lineWidth=width_of_line;
+ ctx.moveTo(last_position_of_x,last_position_of_y)   
+ ctx.lineTo (current_position_of_mouse_x,current_position_of_mouse_y)
+ ctx.stroke();
+}
+last_position_of_x=current_position_of_mouse_x
+last_position_of_y=current_position_of_mouse_y
+}
+function clear() {
+    canvas.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+}
